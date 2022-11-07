07145a88b81d5daa8377ee6f2917fadf77ff5f78
Nayla Nur Amalia <95407345+nylaam@users.noreply.github.com>
2022-11-07 09:19:19 +0700

    Update praktikum.js

--- praktikum.js
+++ praktikum.js
@@ -235,3 +235,20 @@ app.post("/bmi", (req,res) => {
     }
     res.json(response)
 })
+//no 5 
+app.post("/ganjilgenap", (req,res) => {
+    let nilai = Number (req.body.angka)
+    let angka=''    
+   
+let status = ''
+    if (nilai%2==0) {
+        status= "genap"
+    } else if(nilai%2==1){
+        status= "ganjil"
+    } 
+    let response = {
+        Angka:angka,
+        status:status 
+    }
+    res.json(response)
+})
